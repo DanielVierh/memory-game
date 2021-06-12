d317087899fe27f001480aad29cb4a16e76f5389
DanielVierh <DanielVierh@users.noreply.github.com>
2021-06-12 12:09:34 +0200

    Projekt am 11.06.21 begonnen. Very easy uns easy programmiert

---
+++ app.js
@@ -0,0 +1,32 @@
+
+// GOTO
+
+function gotoVeryEasy() {
+    console.log("Very Easy");
+    window.location = "veryEasy.html"
+}
+
+
+function gotoEasy() {
+    window.location = "easy.html"
+}
+
+
+function gotoMMedium() {
+    console.log("Medium");
+}
+
+
+function gotoHard() {
+    console.log("Hard");
+}
+
+
+function gotoVeryHard() {
+    console.log("Very Hard");
+}
+
+
+function gotoSettings() {
+
+}
